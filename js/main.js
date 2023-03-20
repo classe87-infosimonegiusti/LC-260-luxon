95751f7cb20d082aea5bd7dd53b7223242d46173
Simone Giusti <info@simonegiusti.it>
2023-03-20 10:06:38 +0100

    LC con vue

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+//https://moment.github.io/luxon/#/
+
 const DateTime = luxon.DateTime;
 
-const dataOraInItaliano = DateTime.now().setLocale('it').toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)
 
-console.log(dataOraInItaliano);
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: DateTime.now().setLocale('it').toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS)
+    }
+  }
+}).mount('#app')
